34a2aef292372fc8200d26318d1f996ebb997072
SonicTheCat <paveliliev90@gmail.com>
2019-05-15 17:02:54 +0300

    ALL VIEWS DONE WITH BOOTSTRAP!

--- 08.Web Server - Database- EFCore - Exercise/SIS/IRunesWebApp/Controllers/BaseController.cs
+++ 08.Web Server - Database- EFCore - Exercise/SIS/IRunesWebApp/Controllers/BaseController.cs
@@ -50,9 +50,7 @@
             }
 
             string content = ReplacePlaceholdersInView(path);
-
             this.ViewBag["renderBody"] = content;
-
             var layoutContent = ReplacePlaceholdersInView(GetLayoutPath());
 
             return new HtmlResult(layoutContent, HttpResponseStatusCode.Ok);
