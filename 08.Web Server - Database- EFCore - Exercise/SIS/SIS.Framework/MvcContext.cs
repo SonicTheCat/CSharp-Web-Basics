cc1931473e409d22a6f7e3ed75cfc335fa6edb9d
SonicTheCat <paveliliev90@gmail.com>
2019-05-17 04:22:25 +0300

    MVC CONTEXT CLASS ADDED

--- 08.Web Server - Database- EFCore - Exercise/SIS/SIS.Framework/MvcContext.cs
+++ 08.Web Server - Database- EFCore - Exercise/SIS/SIS.Framework/MvcContext.cs
@@ -7,5 +7,15 @@
         private MvcContext() { }
 
         public static MvcContext Get => Instance ?? (Instance = new MvcContext());
+
+        public string AssemblyName { get; set; }
+
+        public string ControllersFolder { get; set; }
+
+        public string ControllersSuffix { get; set; }
+
+        public string ViewsFolder { get; set; }
+
+        public string ModelsFolder { get; set; }
     }
 }
