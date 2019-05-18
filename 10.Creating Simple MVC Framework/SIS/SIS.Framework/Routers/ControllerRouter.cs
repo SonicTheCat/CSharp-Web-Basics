cee6309ce216a8bfba059ecb6613b5870310171d
SonicTheCat <paveliliev90@gmail.com>
2019-05-18 18:41:01 +0300

    CREATING SIMPLE MVC FRAMEWORK

---
+++ 10.Creating Simple MVC Framework/SIS/SIS.Framework/Routers/ControllerRouter.cs
@@ -0,0 +1,137 @@
+﻿using SIS.Framework.ActionResults.Contracts;
+using SIS.Framework.Attributes;
+using SIS.Framework.Controllers;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Requests.Contracts;
+using SIS.HTTP.Responses;
+using SIS.HTTP.Responses.Contracts;
+using SIS.WebServer.Api.Contracts;
+using SIS.WebServer.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SIS.Framework.Routers
+{
+    public class ControllerRouter : IHttpHandler
+    {
+        private Controller GetController(string controllerName, IHttpRequest request)
+        {
+            if (controllerName != null)
+            {
+                string controllerTypeName = string.Format(
+                    "{0}.{1}.{2}{3}",
+                    MvcContext.Get.AssemblyName,
+                    MvcContext.Get.ControllersFolder,
+                    controllerName,
+                    MvcContext.Get.ControllersSuffix
+                    );
+
+                var assembly = Assembly.Load(MvcContext.Get.AssemblyName);
+
+                var controllerType = assembly.GetType(controllerTypeName);
+
+                var controller = (Controller)Activator.CreateInstance(controllerType);
+
+                if (controller != null)
+                {
+                    controller.Request = request;
+                }
+
+                return controller;
+            }
+
+            return null;
+        }
+
+        private MethodInfo GetAction(string requestMethod, Controller controller, string actionName)
+        {
+            MethodInfo method = null;
+
+            foreach (var methodInfo in this.GetSuitableMethods(controller, actionName))
+            {
+                var attributes = methodInfo
+                    .GetCustomAttributes()
+                    .Where(attribute => attribute is HttpMethodAttribute)
+                    .Cast<HttpMethodAttribute>();
+
+                if (!attributes.Any() && requestMethod.ToUpper() == "GET")
+                {
+                    return methodInfo; 
+                }
+
+                foreach (var attribute in attributes)
+                {
+                    if (attribute.IsValid(requestMethod))
+                    {
+                        return methodInfo;
+                    }
+                }
+            }
+
+            return method;
+        }
+
+        private IEnumerable<MethodInfo> GetSuitableMethods(Controller controller, string actionName)
+        {
+            if (controller == null)
+            {
+                return new MethodInfo[0]; 
+            }
+
+            return controller
+                .GetType()
+                .GetMethods()
+                .Where(methodInfo => methodInfo.Name.ToLower() == actionName.ToLower()); 
+        }
+
+        private IHttpResponse PrepareResponse(Controller controller, MethodInfo action)
+        {
+            IActionResult actionResult = (IActionResult)action.Invoke(controller, null);
+            string invocationResult = actionResult.Invoke();
+
+            if (actionResult is IViewable)
+            {
+                return new HtmlResult(invocationResult, HttpResponseStatusCode.Ok); 
+            }
+            else if (actionResult is IRedirectable)
+            {
+                return new RedirectResult(invocationResult); 
+            }
+            else
+            {
+                throw new InvalidOperationException("The view result is not supported!"); 
+            }
+        }
+
+        public IHttpResponse Handle(IHttpRequest request)
+        {
+            var requestMethod = request.RequestMethod.ToString();
+            var controllerName = string.Empty;
+            var actionName = string.Empty;
+
+            if (request.Url == "/")
+            {
+                controllerName = "Home";
+                actionName = "Index";
+            }
+            else
+            {
+                var requestUrlSplit = request.Url.Split("/", StringSplitOptions.RemoveEmptyEntries);
+                controllerName = requestUrlSplit[0];
+                actionName = requestUrlSplit[1];
+            }
+
+            var controller = this.GetController(controllerName, request);
+            var action = this.GetAction(requestMethod, controller, actionName);
+
+            if (controller == null || action == null)
+            {
+                return new HttpResponse(HttpResponseStatusCode.NotFound);
+            }
+
+            return this.PrepareResponse(controller, action); 
+        }
+    }
+}
---
+++ 10.Creating Simple MVC Framework/SIS/SIS.Framework/Utilities/ControllerUtilities.cs
@@ -0,0 +1,24 @@
+﻿namespace SIS.Framework.Utilities
+{
+    public class ControllerUtilities
+    {
+        private const string RootDirectoryRelativePath = "../../../";
+        private const string fileExtensionsHtml = ".html";
+
+        public static string GetControllerName(object controller)
+        {
+            return controller
+                .GetType()
+                .Name
+                .Replace(MvcContext.Get.ControllersSuffix, string.Empty);
+        }
+
+        public static string GetViewFullQualifiedName(string controller, string viewName)
+        {
+            return string.Format("{0}/{1}/{2}",
+                RootDirectoryRelativePath + MvcContext.Get.ViewsFolder,
+                controller,
+                viewName + fileExtensionsHtml);
+        }
+    }
+}
